c5b69cc7f175d63fa55550816933f9881009e866
kmj0424 <kimminji030424@gmail.com>
2025-11-07 20:21:26 +0900

    상점

--- InMarket.c
+++ InMarket.c
@@ -2,27 +2,48 @@
 
 void	ActPurchase(t_Player *Player)
 {
-	Printf("(1)체력포션\n");
+	int	Input;
+
+	while (1)
+	{
+		printf("(1)체력포션 : 100G, (2)마나포션 : 100G (3)나가기\n");
+		scanf("%d", &Input);
+		if (Player->Gold < 100)
+		{
+			printf("Gold 부족\n");
+			return ;
+		}
+		switch (Input)
+		{
+			case 1:
+			{
+				Player->Gold -= 100;
+				Player->Inv.HpPotion += 1;
+			}
+			case 2:
+			{
+				Player->Gold -= 100;
+				Player->Inv.MpPotion += 1;
+			}
+			default:
+				return ;
+		}
+	}
 }
 
 void    InMarket(t_Player *Player)
 {
 	int Input;
 
-	printf("(1)구매 (2)나가기");
+	printf("(1)구매 (2)나가기\n");
 	scanf("%d", &Input);
 	switch (Input)
 	{
-		case Purchase:
-		{        
+		case 1:
+		{
 			ActPurchase(Player);
 			break;
 		}
-		// case Sale:
-		// {
-		// 	ActSale(Player);
-		// 	break;
-		// }
 		default:
 			break;
 	}
